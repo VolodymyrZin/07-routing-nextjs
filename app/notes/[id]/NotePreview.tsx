52c97e1dc32b8b30af7fa24f9d81895fd1f90cc5
ZinVolodymyr <vl.zinych@gmail.com>
2026-08-02 18:25:15 +0300

    fix modal2

--- app/notes/[id]/NotePreview.tsx
+++ app/notes/[id]/NotePreview.tsx
@@ -3,7 +3,7 @@
 import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { fetchNoteById } from '@/lib/api';
-import css from './NoteDetails.module.css';
+import css from './NotePreview.module.css';
 
 export default function NoteDetailsClient() {
   const params = useParams();
